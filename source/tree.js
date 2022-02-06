a0b5aaef2d7925bd7e0f78f8760d9eb265e798c5
krulsaidme0w <mr.priluskiy.2002@mail.ru>
2022-02-07 00:12:22 +0300

    tree function added

---
+++ source/tree.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function tree(height) {
+
+    if(height < 3)
+        return null;
+
+    let width = 1;
+    for(let i = 1; i < height - 1; i++) {
+        width += 2;
+    }
+
+    let tree = "";
+    let length = 1;
+    let gap = (width - length) / 2;
+    for(let i = 0; i < height - 1; ++i) {
+        tree += ' '.repeat(gap) + '*'.repeat(length) + ' '.repeat(gap) + '\n';
+        length += 2;
+        gap = (width - length) / 2;
+    }
+    tree += ' '.repeat((width - 1) / 2) + '|' + ' '.repeat((width - 1) / 2) + '\n';
+
+    return tree;
+};
