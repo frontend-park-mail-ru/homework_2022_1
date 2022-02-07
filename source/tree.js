07356be07a9872f37aa0f91bb2749ce0301ea742
Andrey123815 <ivredomon@gmail.com>
2022-02-08 02:34:01 +0300

    using closures, deleting a global variable, global refactoring

--- source/tree.js
+++ source/tree.js
@@ -1,68 +1,60 @@
 'use strict';
 
-let final_string = ''
-
-
-/**
- * @function Высчитывает нужное количество пробелов по номеру строки и добавляет в результат.
- * @param {number} lineNumber - номер строки, для которой рассчитывается нужное количество пробелов.
- * @param {number} treeSize - размер ASCII-дерева.
- */
-function drawSpaces(lineNumber, treeSize)  {
-    let max_count_spaces = treeSize - 1;
-    final_string += ' '.repeat(max_count_spaces - lineNumber);
-}
-
-
-/**
- * @function Высчитывает нужное количество символов '*' по номеру строки и добавляет в результат.
- * @param {number} lineNumber - номер строки, для которой рассчитывается нужное количество символов '*'.
- */
-function drawStars(lineNumber) {
-    final_string += '*'.repeat(lineNumber * 2 - 1);
-}
-
-
-/**
- * @function Отрисовывает ASCII-дерево по горизонтальным линиям.
- * @param {number} lineNumber - номер отрисовываемой строки.
- * @param {number} treeSize - размер ASCII-дерева.
- */
-function drawLine(lineNumber, treeSize) {
-    drawSpaces(lineNumber, treeSize);
-    drawStars(lineNumber);
-    drawSpaces(lineNumber, treeSize);
-    final_string += '\n';
-}
-
-
-/**
- * @function Отрисовывает корень дерева (представленный символом '|') с учетом пробелов справа и слева.
- * @param {number} treeSize - размер ASCII-дерева.
- */
-function drawRoot(treeSize) {
-    drawSpaces(1, treeSize);
-    final_string += '|';
-    drawSpaces(1, treeSize);
-    final_string += '\n';
-}
-
 
 /**
  * @function Отрисовывает полностью ASCII-дерево с учетом "листьев" и "корня".
  * @param {number} treeSize - размер ASCII-дерева.
  */
 function drawTree(treeSize) {
+    let final_string = '';
+
+    /**
+     * @function Отрисовывает ASCII-дерево по горизонтальным линиям.
+     * @param {number} lineNumber - номер отрисовываемой строки.
+     */
+    function drawLine(lineNumber) {
+
+        /**
+         * @function Высчитывает нужное количество символов '*' по номеру строки и добавляет в результат.
+         */
+        function drawStars() {
+            final_string += '*'.repeat(lineNumber * 2 - 1);
+        }
+
+        /**
+         * @function Высчитывает нужное количество пробелов по номеру строки и добавляет в результат.
+         */
+        function drawSpaces()  {
+            let max_count_spaces = treeSize - 1;
+            final_string += ' '.repeat(max_count_spaces - lineNumber);
+        }
+
+        drawSpaces();
+        drawStars();
+        drawSpaces();
+
+        final_string += '\n';
+    }
+
+    /**
+     * @function Отрисовывает корень дерева (представленный символом '|') с учетом пробелов справа и слева.
+     */
+    function drawRoot() {
+        let spaces = ' '.repeat(treeSize - 2);
+        final_string += spaces + '|' + spaces + '\n';
+    }
+
     final_string = '';
 
     for (let i = 1; i < treeSize; ++i) {
-        drawLine(i, treeSize);
+        drawLine(i);
     }
 
-    drawRoot(treeSize);
+    drawRoot();
 
     return final_string;
 }
+
 
 const tree = (treeSize) => {
     if (treeSize < 3 || treeSize === undefined || !(treeSize % 1 === 0)) {
