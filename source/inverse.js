eab0b537f46aa7d18aa4de0adb1fc186f9cec76a
grv.nikita <sitleman68@gmail.com>
2022-02-07 22:25:46 +0300

    add 1jsDoc

--- source/inverse.js
+++ source/inverse.js
@@ -1,10 +1,15 @@
-"use strict";
+"use strict";  /** modern mode connection */
 
+/** @function inverse */
 const inverse = function(arr, arg = 0) {
+    /** find border of array for reverse */
     let left = Math.max(arg, 0);
     let right = Math.min(arr.length + arg, arr.length);
+
+    /** reverse array */
     for (let i = left; i < (left + right) / 2; i++){
-        [arr[i], arr[right - (i - left) - 1]] = [arr[right - (i - left) - 1], arr[i]]  //swap
+        /** swap elements */
+        [arr[i], arr[right - (i - left) - 1]] = [arr[right - (i - left) - 1], arr[i]];
     }
     return arr;
 }
