509f2225dda0d68078122346cc817a2962d7d2dd
Andrey123815 <ivredomon@gmail.com>
2022-02-07 21:12:42 +0300

    add JSDoc

--- source/tree.js
+++ source/tree.js
@@ -2,6 +2,12 @@
 
 let final_string = ""
 
+
+/**
+ * @function Высчитывает нужное количество пробелов по номеру строки и добавляет в результат.
+ * @param {number} line_number - номер строки, для которой рассчитывается нужное количество пробелов.
+ * @param {number} tree_size - размер ASCII-дерева.
+ */
 function drawSpaces(line_number, tree_size)  {
     let max_count_spaces = tree_size - 1;
     for (let i = 0; i < max_count_spaces - line_number; ++i) {
@@ -9,19 +15,23 @@ function drawSpaces(line_number, tree_size)  {
     }
 }
 
+
+/**
+ * @function Высчитывает нужное количество символов '*' по номеру строки и добавляет в результат.
+ * @param {number} line_number - номер строки, для которой рассчитывается нужное количество символов '*'.
+ */
 function drawStars(line_number) {
     for (let i = 0; i < line_number * 2 - 1; ++i) {
         final_string += "*";
     }
 }
 
-function drawRoot(tree_size) {
-    drawSpaces(1, tree_size);
-    final_string += "|";
-    drawSpaces(1, tree_size);
-    final_string += "\n"
-}
 
+/**
+ * @function Отрисовывает ASCII-дерево по горизонтальным линиям.
+ * @param {number} line_number - номер отрисовываемой строки.
+ * @param {number} tree_size - размер ASCII-дерева.
+ */
 function drawLine(line_number, tree_size) {
     drawSpaces(line_number, tree_size);
     drawStars(line_number);
@@ -29,7 +39,26 @@ function drawLine(line_number, tree_size) {
     final_string += "\n";
 }
 
+
+/**
+ * @function Отрисовывает корень дерева (представленный символом '|') с учетом пробелов справа и слева.
+ * @param {number} tree_size - размер ASCII-дерева.
+ */
+function drawRoot(tree_size) {
+    drawSpaces(1, tree_size);
+    final_string += "|";
+    drawSpaces(1, tree_size);
+    final_string += "\n"
+}
+
+
+/**
+ * @function Отрисовывает полностью ASCII-дерево с учетом "листьев" и "корня".
+ * @param {number} tree_size - размер ASCII-дерева.
+ */
 function drawTree(tree_size) {
+    final_string = "";
+
     if (tree_size < 3 || tree_size === undefined || !(tree_size % 1 === 0)) {
         return null;
     }
@@ -40,10 +69,7 @@ function drawTree(tree_size) {
 
     drawRoot(tree_size);
 
-    let result = final_string
-    final_string = ""
-
-    return result;
+    return final_string;
 }
 
 const tree = tree_size => drawTree(tree_size);
