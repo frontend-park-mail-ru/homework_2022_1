d7fa1a89f51f9cee7c55be1adead48b0a970b543
vorolga <voronovaolja2001@mail.ru>
2022-02-08 18:09:25 +0300

    add 1JSDoc

--- source/euclid.js
+++ source/euclid.js
@@ -1,5 +1,21 @@
 'use strict';
 
+/**
+ * Find GCD of natural numbers.
+ * @function euclid
+ * @param {...number} numbers - input array of natural numbers.
+ * @returns {number}
+ * @example
+ * euclid(2);
+ * // returns 2
+ * @example
+ * euclid(7, 14, 21);
+ * // returns 7
+ * @example
+ * euclid(13, 26, 5);
+ * // returns 1
+ */
+
 const euclid = (...numbers) => {
     if (numbers.length === 0) {
         throw Error('There are no arguments');
