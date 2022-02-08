bfa00fbad29869c3f3c71ec0718c50b80b3d2326
Andrey123815 <ivredomon@gmail.com>
2022-02-09 00:59:00 +0300

    tests/tree.js code style change

--- tests/tree.js
+++ tests/tree.js
@@ -1,87 +1,87 @@
 'use strict';
 
 QUnit.module('Тестируем функцию tree', function () {
-    QUnit.test('Ёлочек высотой ниже трёх не бывает', function (assert) {
-		assert.strictEqual(tree(0), null);
-		assert.strictEqual(tree(1), null);
-		assert.strictEqual(tree(2), null);
-		assert.strictEqual(tree('0'), null);
-		assert.strictEqual(tree('1'), null);
-		assert.strictEqual(tree('2'), null);
-    });
+		QUnit.test('Ёлочек высотой ниже трёх не бывает', function (assert) {
+                assert.strictEqual(tree(0), null);
+                assert.strictEqual(tree(1), null);
+                assert.strictEqual(tree(2), null);
+                assert.strictEqual(tree('0'), null);
+                assert.strictEqual(tree('1'), null);
+                assert.strictEqual(tree('2'), null);
+        });
 
-    QUnit.test('Ёлочек с высотой типа undefined не бывает', function (assert) {
-        assert.strictEqual(tree(undefined), null);
-        assert.strictEqual(tree('undefined'), null);
-    });
+        QUnit.test('Ёлочек с высотой типа undefined не бывает', function (assert) {
+                assert.strictEqual(tree(undefined), null);
+                assert.strictEqual(tree('undefined'), null);
+        });
 
-    QUnit.test('Ёлочек с высотой типа null не бывает', function (assert) {
-	assert.strictEqual(tree(null), null);
-	assert.strictEqual(tree('null'), null);
-    });
+        QUnit.test('Ёлочек с высотой типа null не бывает', function (assert) {
+	            assert.strictEqual(tree(null), null);
+	            assert.strictEqual(tree('null'), null);
+        });
 
-    QUnit.test('Ёлочек с высотой нецелого типа не бывает', function (assert) {
-        assert.strictEqual(tree(1.1), null);
-        assert.strictEqual(tree(2.2), null);
-        assert.strictEqual(tree(3.3), null);
-        assert.strictEqual(tree('1.1'), null);
-        assert.strictEqual(tree('2.2'), null);
-        assert.strictEqual(tree('3.3'), null);
-    });
+        QUnit.test('Ёлочек с высотой нецелого типа не бывает', function (assert) {
+                assert.strictEqual(tree(1.1), null);
+                assert.strictEqual(tree(2.2), null);
+                assert.strictEqual(tree(3.3), null);
+                assert.strictEqual(tree('1.1'), null);
+                assert.strictEqual(tree('2.2'), null);
+                assert.strictEqual(tree('3.3'), null);
+        });
 
-    QUnit.test('Ёлочка высотой 3', function (assert) {
-	const expected =
-		' * \n' +
-		'***\n' +
-		' | \n';
-	assert.strictEqual(tree(3), expected);
-	assert.strictEqual(tree('3'), expected);
-    });
+        QUnit.test('Ёлочка высотой 3', function (assert) {
+	            const expected =
+		                ' * \n' +
+		                '***\n' +
+		                ' | \n';
+	            assert.strictEqual(tree(3), expected);
+	            assert.strictEqual(tree('3'), expected);
+        });
 
-    QUnit.test('Ёлочка высотой 4', function (assert) {
-	const expected =
-		'  *  \n' +
-		' *** \n' +
-		'*****\n' +
-		'  |  \n';
-	assert.strictEqual(tree(4), expected);
-	assert.strictEqual(tree('4'), expected);
-    });
+        QUnit.test('Ёлочка высотой 4', function (assert) {
+	            const expected =
+		                '  *  \n' +
+		                ' *** \n' +
+		                '*****\n' +
+		                '  |  \n';
+	            assert.strictEqual(tree(4), expected);
+	            assert.strictEqual(tree('4'), expected);
+        });
 
-    QUnit.test('Ёлочка высотой 5', function (assert) {
-	const expected =
-		'   *   \n' +
-		'  ***  \n' +
-		' ***** \n' +
-		'*******\n' +
-		'   |   \n';
-	assert.strictEqual(tree(5), expected);
-	assert.strictEqual(tree('5'), expected);
-    });
+        QUnit.test('Ёлочка высотой 5', function (assert) {
+	            const expected =
+		                '   *   \n' +
+		                '  ***  \n' +
+		                ' ***** \n' +
+		                '*******\n' +
+		                '   |   \n';
+	            assert.strictEqual(tree(5), expected);
+	            assert.strictEqual(tree('5'), expected);
+        });
 
-    QUnit.test('Ёлочка высотой 6', function (assert) {
-	const expected =
-		'    *    \n' +
-		'   ***   \n' +
-		'  *****  \n' +
-		' ******* \n' +
-		'*********\n' +
-		'    |    \n';
-	assert.strictEqual(tree(6), expected);
-	assert.strictEqual(tree('6'), expected);
-    });
+        QUnit.test('Ёлочка высотой 6', function (assert) {
+	            const expected =
+		                '    *    \n' +
+		                '   ***   \n' +
+		                '  *****  \n' +
+		                ' ******* \n' +
+		                '*********\n' +
+		                '    |    \n';
+	            assert.strictEqual(tree(6), expected);
+	            assert.strictEqual(tree('6'), expected);
+        });
 
-    QUnit.test('Ёлочка высотой 8', function (assert) {
-	const expected =
-		'      *      \n' +
-		'     ***     \n' +
-		'    *****    \n' +
-		'   *******   \n' +
-		'  *********  \n' +
-		' *********** \n' +
-		'*************\n' +
-		'      |      \n';
-	assert.strictEqual(tree(8), expected);
-	assert.strictEqual(tree('8'), expected);
-    });
+        QUnit.test('Ёлочка высотой 8', function (assert) {
+	            const expected =
+		                '      *      \n' +
+                        '     ***     \n' +
+		                '    *****    \n' +
+		                '   *******   \n' +
+		                '  *********  \n' +
+		                ' *********** \n' +
+		                '*************\n' +
+		                '      |      \n';
+	            assert.strictEqual(tree(8), expected);
+	            assert.strictEqual(tree('8'), expected);
+        });
 });
