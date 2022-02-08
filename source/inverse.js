e373f353b178ce32b0f5e3d0eeca0051c1328d9e
nezhibitskiy <cross01rus@icloud.com>
2022-02-08 17:43:34 +0300

    Fixed comments

--- source/inverse.js
+++ source/inverse.js
@@ -1,22 +1,42 @@
 'use strict';
 
+/**
+ * Inverse elements in array.
+ * @param {Array<>} array - Array for inversing
+ * @param {number} index - Index of unchangeable part of array (if negative, counts from end of array)
+ */
 const inverse = function (array, index = 0) {
+    /** Check if index is default */
     if (index == 0) return array.reverse();
 
-    if (index > 0) 
+    if (index > 0)
     {
+        /** 
+         * If index is positive, reverse only rigth part
+         * Get new array from unchangeable part.
+         */
         let newArr = array.slice(0, index);
+        /** Delete unchangeable part from origial array */
         array.splice(0, index);
+        /** Reverse left part of array */
         array.reverse();
+        /** Concatenate unchangeable part and reversed part of original array */
         array = newArr.concat(array);
         return array;
     }
-    else 
+    else
     {
+        /**
+         * If index is negative, reverse only left part
+         * Get new array from unchangeable part.
+        */
         let newArr = array.slice(index);
+        /** Delete unchangeable part from origial array */
         array.splice(index);
+        /** Reverse left part of array */
         array.reverse();
+        /** Concatenate unchangeable part and reversed part of original array */
         array = array.concat(newArr);
         return array;
     }
-};
+}
