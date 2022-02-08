699fbdd6f8a5f3bcd5839c52a3e4e2b72729c443
iiivan-lemon <lipton2807@yandex.ru>
2022-02-08 13:44:35 +0300

    jsdoc correction

--- source/plain.js
+++ source/plain.js
@@ -1,10 +1,14 @@
 'use strict';
 
-/* 
-merging arrays into one
-
-@params {Object[]} - nested(source) array 
-@returns {Object[]} - plain array
+/** 
+*  merging arrays into one
+*
+*  @params {Object[]} - nested(source) array 
+*  @returns {Object[]} - plain array
+*  @example
+ * // returns [1,2,3]
+ * plain([1,[2,[3]]]);
+* 
 */
 
 const plain = arr => arr.flat(Infinity);
