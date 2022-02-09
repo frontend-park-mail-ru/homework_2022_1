37d8edb636f39dc05b3ec993b3d2032a11e55415
iiivan-lemon <lipton2807@yandex.ru>
2022-02-09 16:17:41 +0300

    delete unnecessary

--- source/plain.js
+++ source/plain.js
@@ -3,8 +3,8 @@
 /** 
 *  merging arrays into one
 *
-*  @params {Array[]} - nested(source) array 
-*  @returns {Array[]} - plain array
+*  @params {Array} - nested(source) array 
+*  @returns {Array} - plain array
 *  @example
  * // returns [1,2,3]
  * plain([1,[2,[3]]]);
