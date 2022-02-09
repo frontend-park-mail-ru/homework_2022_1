539dad57300a1eed6e68791bb9e5f5b3080c9173
iiivan-lemon <lipton2807@yandex.ru>
2022-02-10 00:52:01 +0300

    my own flat-func

--- source/plain.js
+++ source/plain.js
@@ -3,12 +3,30 @@
 /** 
 *  merging arrays into one
 *
-*  @params {Array} - nested(source) array 
-*  @returns {Array} - plain array
+*  @params {Array} - arr - nested(source) array 
+*  @returns {Array} - arrMerg - plain array
 *  @example
  * // returns [1,2,3]
  * plain([1,[2,[3]]]);
 * 
 */
 
-const plain = arr => arr.flat(Infinity);
+const plain = arr => {
+
+    let arrMerg = [];
+
+    for (let i = 0; i < arr.length; ++i) {
+
+        if (Array.isArray(arr[i])) {
+
+            arrMerg = arrMerg.concat(plain(arr[i]));
+
+        } else {
+
+            arrMerg.push(arr[i]);
+
+        }
+    }
+
+    return arrMerg;
+}
