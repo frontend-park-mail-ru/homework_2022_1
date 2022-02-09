c712a493c878a687f675c8696636e9031a7fe747
Danya <danyat02@yandex.ru>
2022-02-10 01:41:48 +0300

    Add fixed

--- source/letters.js
+++ source/letters.js
@@ -10,35 +10,33 @@
  * @returns {string}
  * */
 
-
-function letters(str, mode) {
+let letters = (str, mode) => {
     let arr = str.split('');
 
-    if (mode === true) {
-        arr = arr.filter((item, index) => arr.indexOf(item) === index);
-        str = arr.join('');
-    } else if (mode === false) {
-        arr = arr.filter((item, index) => arr.lastIndexOf(item) === index);
-        str = arr.join('');
-    } else {
+    if (mode === undefined) {
+        let counter = {};
+
+        for (let item of arr) {
+            (item in counter) ? counter[item]++ : counter[item] = 1;
+        }
+
         let result = [];
-        let stack = [];
 
-        for (let i = 0; i < arr.length; ++i) {
-            for (let j = i + 1; j < arr.length; ++j) {
-                if (arr[i] === arr[j]) {
-                    stack.push(arr[i])
-                }
-            }
-
-            if (stack.find(item => item === arr[i]) === undefined) {
-                console.log(arr[i])
-                result.push(arr[i]);
+        for (let item of arr) {
+            if (counter[item] === 1) {
+                result.push(item);
             }
         }
 
         str = result.join('');
+
+        return str;
     }
 
+    arr = (mode) ? arr.filter((item, index) => arr.indexOf(item) === index) :
+        arr.filter((item, index) => arr.lastIndexOf(item) === index);
+
+    str = arr.join('');
+
     return str;
-}
+};
