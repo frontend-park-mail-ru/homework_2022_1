f4317c0b4fd07003526ffe6160a8f1fbb4465133
krulsaidme0w <mr.priluskiy.2002@mail.ru>
2022-02-09 11:51:53 +0300

    better doc

--- source/tree.js
+++ source/tree.js
@@ -1,28 +1,51 @@
 'use strict';
 
 /**
- * Validate input
+ * Check if is integer
  * @function isInt
  * @param {number} data - input data.
  * @returns {(number|null)} returns true if data is int.
- * 
+ */
+function isInt(data) {
+    if (Number(data) === parseInt(data)) {
+        return data;
+    }
+    return null;
+}
+
+/**
  * Count tree width
  * @function countWidth
  * @param {number} height - input height.
  * @returns {number} returns width.
- * 
+ */
+function countWidth(height) {
+    return height * 2 - 3;
+}
+
+/**
+ * Return trunk line
+ * @function getTrunk
+ * @param {number} height - input height.
+ * @returns {number} returns width.
+ */
+function getTrunk(width) {
+    return ' '.repeat((width - 1) / 2) + '|' + ' '.repeat((width - 1) / 2) + '\n';
+}
+
+/**
  * Returns every line of tree (not a trunk line)
  * @function getRow
  * @param {number} gap - tree ' ' in one side count.
  * @param {number} length - tree '*' count.
  * @returns {number} returns width.
- * 
- * Return trunk line
- * @function getTrunk
- * @param {number} height - input height.
- * @returns {number} returns width.
- * 
- * Print tree.
+ */
+function getRow(gap, length) {
+    return ' '.repeat(gap) + '*'.repeat(length) + ' '.repeat(gap) + '\n';
+}
+
+/**
+ * Return tree.
  * @function tree
  * @param {number} width - input tree width.
  * @returns {string} return string with width length with '|' in middle.
@@ -35,26 +58,6 @@
  *    |
  *`
  */
-
-function isInt(data) {
-    if (Number(data) === parseInt(data)) {
-        return data;
-    }
-    return null;
-}
-
-function countWidth(height) {
-    return height * 2 - 3;
-}
-
-function getTrunk(width) {
-    return ' '.repeat((width - 1) / 2) + '|' + ' '.repeat((width - 1) / 2) + '\n';
-}
-
-function getRow(gap, length) {
-    return ' '.repeat(gap) + '*'.repeat(length) + ' '.repeat(gap) + '\n';
-}
-
 function tree(height) {
 
     height = isInt(height) > 2 ? height : null;
