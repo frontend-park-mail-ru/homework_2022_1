a112420e669c6f4e6b2535fee4b91a1bf2b65d67
Nikita Schelkanov <nik.schelkanov@gmail.com>
2022-02-09 16:55:52 +0300

    initial commit

---
+++ source/format.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const format = (numbers, columnNumbers) => {
+    let highestMultiIndent = [];
+    let output = '';
+    let row = 0;
+
+    for (let j = 0; j < columnNumbers; j++) {
+        let highestIndent = 0;
+        for (let i = (0+j); i < numbers.length; i+=columnNumbers) {
+            highestIndent = (`${numbers[i]}`.length > highestIndent)? `${numbers[i]}`.length : highestIndent; 
+        }
+        highestMultiIndent[j] = highestIndent;
+    }
+
+    for (let i = 0; i < numbers.length; i+=columnNumbers) {
+        for (let j = 0; j < columnNumbers; j++) {
+            if (j == 0) {
+                if (numbers[i + j] !== undefined) {
+                    let length = `${numbers[row + j]}`.length
+                    output += (' '.repeat(highestMultiIndent[j]  - `${numbers[i + j]}`.length) + numbers[i + j]);
+                }
+            } else {
+                if (numbers[i + j] !== undefined) {
+                    let length = `${numbers[row + j]}`.length
+                    output += (' '.repeat(highestMultiIndent[j] + 1 - `${numbers[i + j]}`.length) + numbers[i + j]);
+                }
+            }
+        }
+        if ((i + columnNumbers) < numbers.length)
+            output += '\n';
+    }
+    return (output);
+}
