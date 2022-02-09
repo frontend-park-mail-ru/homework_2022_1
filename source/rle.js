1a22d31a1660ac244241e6f75cc7973151933c99
Kislv <vityakiselev96@gmail.com>
2022-02-09 04:09:19 +0300

    After review №1

--- source/rle.js
+++ source/rle.js
@@ -1,31 +1,41 @@
+/** @global
+ *  @constant
+ *  @type {string}
+ *  @default
+*/
+const validationError = 'Argument is not a string';
 /**
  * The function implements repeat encoding (data compression), replacing repeated characters with one character and its number of repetitions
  * @param {string} str Is the string that you want to convert. For example: 'ABBCCCC'
  * @returns {string} the transformed string. For example: 'AB2C4'
  */
- const rle = function (str) {
-    try {
-      if (typeof (str) !== 'string') {
-        throw new TypeError('The argument must be of type "string"');
-      }
-    } catch (error) {
-      console.error(error);
-    }
-    let resultStr = str.charAt(0);
-    let counter = 1;
-    for (let i = 1; i < str.length; ++i) {
-      if (str.charAt(i - 1) == str.charAt(i)) {
-        ++counter;
-      } else if (counter == 1) {
-        resultStr += str.charAt(i);
-      } else {
-        resultStr += counter + str.charAt(i);
+const rle = function (str) {
+  if (typeof (str) !== 'string') {
+    return validationError;
+  }
+  let counter = 1;
+  let previousCharacter = '';
+  const { map } = Array.prototype;
+  const resultStr = map.call(str, (currentCharacter) => {
+    if (previousCharacter == currentCharacter) {
+      if (counter == 9) {
         counter = 1;
+        return (`9${currentCharacter}`);
       }
-      if (i == str.length - 1 && counter > 1) {
-        resultStr += counter;
-      }
+      ++counter;
+      return '';
     }
-    return resultStr;
-  };
-  
+    if (counter == 1) {
+      previousCharacter = currentCharacter;
+      return currentCharacter;
+    }
+    const counterResult = counter;
+    counter = 1;
+    previousCharacter = currentCharacter;
+    return (counterResult + currentCharacter);
+  });
+  if (counter > 1) {
+    resultStr[resultStr.length] = counter;
+  }
+  return (resultStr.join(''));
+};
--- tests/rle.js
+++ tests/rle.js
@@ -7,5 +7,10 @@ QUnit.module('Тестируем функцию rle', function () {
 		assert.strictEqual(rle('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD'), 'AV3B3V2XDHJF4D6HA4J3D2SLS3D4');
 		assert.strictEqual(rle('HHHHFCKKKKKKKKKVUUUUUUYDLLLDTNTNFTTTT'), 'H4FCK9VU6YDL3DTNTNFT4');
 		assert.strictEqual(rle('KKKDDDGGKKDMDNNNTJFJFKKKK'), 'K3D3G2K2DMDN3TJFJFK4');
+		assert.strictEqual(rle('AAAAAAAAAAAAAA'), 'A9A5');
+		assert.strictEqual(rle('A1111'), 'A14');
+		assert.strictEqual(rle('BACCCCCCCCCCCCCCCCCCCCSDD'), 'BAC9C9C2SD2');
+		assert.strictEqual(rle(5), validationError);
+		assert.strictEqual(rle(true), validationError);
 	});
 });
