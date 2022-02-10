ffb4319ce0fd4ba89c3d50ad8ac8e08875b3fe4d
iiivan-lemon <lipton2807@yandex.ru>
2022-02-10 18:19:44 +0300

    fix vers plain

--- source/plain.js
+++ source/plain.js
@@ -1,32 +1,19 @@
 'use strict';
 
-/** 
-*  merging arrays into one
-*
-*  @params {Array} - arr - nested(source) array 
-*  @returns {Array} - arrMerg - plain array
-*  @example
+/**
+ *  merging arrays into one
+ *
+ *  @params {Array} - arr - nested(source) array
+ *  @returns {Array} - arrMerg - plain array
+ *  @example
  * // returns [1,2,3]
  * plain([1,[2,[3]]]);
-* 
-*/
-
+ *
+ */
 const plain = arr => {
 
-    let arrMerg = [];
+    return (Array.isArray(arr)) ? arr.reduce(function (arrMerg, curr) {
+        return arrMerg.concat(plain(curr));
+    }, []) : arr;
 
-    for (let i = 0; i < arr.length; ++i) {
-
-        if (Array.isArray(arr[i])) {
-
-            arrMerg = arrMerg.concat(plain(arr[i]));
-
-        } else {
-
-            arrMerg.push(arr[i]);
-
-        }
-    }
-
-    return arrMerg;
 };
