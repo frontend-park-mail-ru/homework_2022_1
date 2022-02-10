dd0ae7242ceb6bbbaed83963c7711c03112c0a9c
grv.nikita <sitleman68@gmail.com>
2022-02-10 20:39:30 +0300

    fixes v3

--- source/inverse.js
+++ source/inverse.js
@@ -1,7 +1,7 @@
-"use strict";  /** modern mode connection */
+'use strict';  /** modern mode connection */
 
 /**
- * @function - Returns the invesed array except for the elements (from begin / to end) specified in the argument.
+ * Returns the invesed array except for the elements (from begin / to end) specified in the argument.
  * @param {array} arr - Array of elements.
  * @param {number} arg - Count of elements that don't need to be inversed.
  * @returns {array}
@@ -9,20 +9,20 @@
 
 const inverse = (arr, arg = 0) => {
     /** input validation */
-    if (!Number.isInteger(arg) || !Array.isArray(arr)) return ['incorrect arguments'];
+    if (!Array.isArray(arr) || !Number.isInteger(arg)){
+        throw 'Error: incorrect arguments';
+    }
 
     /** find border of array for reverse */
     let left = Math.max(arg, 0);
     let right = Math.min(arr.length + arg, arr.length);
 
     /** reverse array */
-    const invArr = arr.map((item, index, arr) => {
-        if (left <= index && index < right){
+    return arr.map((item, index, arr) => {
+        if (left <= index && index < right) {
             return arr[right + left - index - 1];
         } else {
             return item;
         }
     });
-
-    return invArr;
 }
