aca5f22093a319204e718d2a73888cc01eb6c79c
Victorius19 <post@victorius.ru>
2022-02-10 20:48:48 +0000

    fixed

--- source/plain.js
+++ source/plain.js
@@ -1,3 +1,5 @@
 'use strict';
 
-const plain = arr => arr.flat(Infinity);
+const plain = arr => {
+    return arr.reduce((acc, el) => Array.isArray(el) ? [...acc, ...plain(el)] : [...acc, el], []);
+}
