59e83395eb4b3e5e570e8e1ae5048b286b829736
Victorius19 <post@victorius.ru>
2022-02-10 21:13:19 +0000

    Add JSDoc

--- source/plain.js
+++ source/plain.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const plain = arr => {
+/**
+ *  Converting a multidimensional array to a single array
+ *
+ *  @params {Array} - multidimensional array
+ *  @returns {Array} - a one-dimensional array with all the values ​​of the original
+ */
+
+const plain = (arr) => {
     return arr.reduce((acc, el) => Array.isArray(el) ? [...acc, ...plain(el)] : [...acc, el], []);
 }
