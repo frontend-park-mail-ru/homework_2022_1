7330b033570ea07467397a5efff5546aef280463
erik770 <mr.erik770@mail.ru>
2022-02-10 17:01:45 +0300

    jsdoc fix

--- source/anagram.js
+++ source/anagram.js
@@ -2,8 +2,7 @@
 
 /**
  * Checking if input is correct (array of strings)
- * @function isValidInput
- * @param {*} words
+ * @param {*} words input data which requires validation for correct future usage
  * @returns {boolean} the result of validation (true if input is correct, false if input is incorrect)
  */
 const isValidInput = (words) => {
@@ -11,7 +10,7 @@ const isValidInput = (words) => {
         return false;
     }
     words.forEach((word) => {
-        if (typeof (word) != "string") {
+        if (typeof (word) !== "string") {
             return false
         }
     })
@@ -20,19 +19,19 @@ const isValidInput = (words) => {
 
 /**
  * Creating a key from the word
- * @function wordToKey
- * @param {string} word
+ * @param {string} word the word from which we are genereting the key 
  * @returns {string} the key composed of sorted word characters
- * @example wordToKey(cadb);
- *          returns abcd
+ * @example wordToKey('cadb');
+ *          returns 'abcd'
  */
 const wordToKey = (word) => word.split('').sort().join('');
 
 /**
- * Finding anagrams in array of strings
- * @function anagram
- * @param {string[]} words 
- * @returns {object} array of arrays with anagrams
+ * Finding anagrams in array of strings  
+ * @param {string[]} words array of words(string) where we are searching anagrams
+ * @returns {string[string[]]} sorted array of arrays with anagrams
+ * @example anagram(['cadb', 'bdac', 'edf', 'abc', 'def']);
+ *          returns [['bdac', 'cadb'], ['def', 'edf']]
  */
 const anagram = (words) => {
     if (!isValidInput(words)) {
