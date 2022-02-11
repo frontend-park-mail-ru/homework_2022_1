d6706c97486aa3cc270df690354a1ea079249a54
Aleksandr <aleksandrnaumenko@mail.ru>
2022-02-12 00:34:44 +0300

    исправил ошибки

--- source/roman.js
+++ source/roman.js
@@ -1,55 +1,62 @@
 'use strict';
 
+/**
+ * @author Naumenko A.A.
+ * @param {number, string} number - Переводимое число
+ * @description Функция приобразования из римских цифр в десятичную систему и обратно
+ */
 
-function roman(number) {
-  let dataset = {
-    "M": 1000,
-    "CM": 900,
-    "D": 500,
-    "CD": 400,
-    "C": 100,
-    "XC": 90,
-    "L": 50,
-    "XL": 40,
-    "X": 10,
-    "IX": 9,
-    "V": 5,
-    "IV": 4,
-    "I": 1
-  }
 
-  let result = "";
-  if (typeof(number) == "number"){
-    for (var key of Object.keys(dataset)) {
-      var temp = Math.floor(number / dataset[key]);
-      number -= temp * dataset[key];
-      result += key.repeat(temp);
+let roman = (number) => {
+    const dataset = {
+        "M": 1000,
+        "CM": 900,
+        "D": 500,
+        "CD": 400,
+        "C": 100,
+        "XC": 90,
+        "L": 50,
+        "XL": 40,
+        "X": 10,
+        "IX": 9,
+        "V": 5,
+        "IV": 4,
+        "I": 1
     }
-  }
-  else if(typeof(number) == "string"){
-    const re = /\d+/;
-    number = number.toUpperCase();
-    if (number.match(re)) {
-      for (var key of Object.keys(dataset)) {
-        var temp = Math.floor(number / dataset[key]);
-        number -= temp * dataset[key];
-        result += key.repeat(temp);
-      }
-    }
-    else {
-      var res = 0;
-      var prevnum = dataset[number.charAt(0)];
-      for (var i = 1; i < number.length; i++) {
-        var temp = dataset[number.charAt(i)];
-        if (temp <= prevnum)
-          res += prevnum;
-        else
-          res -= prevnum;
+
+    return (typeof (number) == "number") ? numtorom(dataset, number) :
+        (typeof (number) == "string") ? strtores(dataset, number) :
+            ''
+}
+
+let strtores = (dataset, number) => {
+    const re_int = /^[0-9]+$/;
+    const re_roman = /^[MDCLXVI]+$/;
+    let upnumber = number.toUpperCase();
+    return (re_int.test(upnumber)) ? numtorom(dataset, number) :
+        (re_roman.test(upnumber)) ? romtonum(dataset, upnumber) :
+            ''
+}
+
+let romtonum = (dataset, upnumber) => {
+    let res = 0;
+    let prevnum = dataset[upnumber.charAt(0)];
+    for (let i = 1; i < upnumber.length; i++) {
+        let temp = dataset[upnumber.charAt(i)];
+        temp <= prevnum ? res += prevnum : res -= prevnum;
         prevnum = temp;
-      }
-      res += prevnum;
-      return res;
     }
-  }
-  return result;
+    res += prevnum;
+    return res;
+}
+
+let numtorom = (dataset, number) => {
+    let result = "";
+    Object.keys(dataset).forEach(key => {
+            let temp = Math.floor(number / dataset[key]);
+            number -= temp * dataset[key];
+            result += key.repeat(temp);
+        }
+    )
+    return result;
 }
