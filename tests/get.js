8783cdb0e106514ee15262b051276acdfb390a85
papazloynt <cool.chastikov53@gmail.com>
2022-02-12 16:10:47 +0300

    chage test

--- tests/get.js
+++ tests/get.js
@@ -14,19 +14,7 @@ QUnit.module('Тестируем функцию get', function () {
          assert.strictEqual(get(obj, 'mother.name'), 'Luda ot verbluda', 'Second level!');
          assert.strictEqual(get(obj, 'name.length'), obj.name.length, 'Write len(name)');
          assert.strictEqual(get(obj, 'father.name'), undefined, 'Undefined!');
-    });
-
-    QUnit.test('передаём в функцию get плохие значения', function (assert) {
-        let obj = {
-            mark: 'audi',
-            model: 'A3',
-            year: 2001,
-            price: {
-                light: 1000000,
-                hard: 2000000
-            }
-        }
-        assert.strictEqual(get(null, 'mark'), undefined, 'Undefined!');
-        assert.strictEqual(get(obj, 15), undefined, 'Undefined!');
+         assert.strictEqual(get(null, 'mark'), undefined, 'Undefined!');
+         assert.strictEqual(get(obj, 15), undefined, 'Undefined!');
     });
 });
