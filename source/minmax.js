23df1f4dd6f8422463a0a69823dba8c40e62e8a7
kdv <kdv200330@bk.ru>
2022-02-12 11:30:17 +0300

    fix #5.1

--- source/minmax.js
+++ source/minmax.js
@@ -1,22 +1,19 @@
-'use strict';
-
 /**
  * find min and max value in string
  * @param {string} value
  * @returns {Array<number>[minValue, maxValue]}
  */
 const minmax = (value) => {
-    if (!(typeof (value) === 'string') && +
-        !(value instanceof String) || (value === '')) {
-        return [undefined, undefined];
-    }
-    const splitValue = value.split(' ').
-        map(string => parseFloat(string)).
-        filter((item) => !Number.isNaN(item));
+  if (!(typeof (value) === 'string' || value instanceof String) || (value === '')) {
+    return [undefined, undefined];
+  }
+  const splitValue = value.split(' ')
+    .map((string) => parseFloat(string))
+    .filter((item) => !Number.isNaN(item));
 
-    return splitValue.reduce(([min = Infinity, max = -Infinity],
-                              current) => {
-        return [(min > current) ? current : min,
-            (max < current) ? current : max];
-    }, [undefined, undefined])
-}
+  return splitValue.reduce((
+    [min = Infinity, max = -Infinity],
+    current,
+  ) => [(min > current) ? current : min,
+    (max < current) ? current : max], [undefined, undefined]);
+};
