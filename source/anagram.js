9930e4aea256471b8aad32918feb2f5ca82e68e3
erik770 <mr.erik770@mail.ru>
2022-02-15 01:40:18 +0300

    hopefully last fixes

--- source/anagram.js
+++ source/anagram.js
@@ -2,32 +2,28 @@
 
 /**
  * Checking if input is correct (array of strings)
- * @param {*} words - input data which requires validation for correct future usage
+ * @param {Array} words - input data which requires validation for correct future usage
  * @returns {boolean} the result of validation (true if input is correct, false if input is incorrect)
  */
 const isValidInput = (words) => {
     if (!Array.isArray(words)) {
         return false;
     }
-    words.forEach((word) => {
-        if (typeof (word) !== "string") {
-            return false
-        }
-    })
-    return true;
+
+    return !words.some(elem => typeof(elem) !== "string");
 }
 
 /**
  * Creating a key from the word
- * @param {string} word - the word from which we are genereting the key 
+ * @param {string} word - the word from which we are genereting the key
  * @returns {string} the key composed of sorted word characters
- * @example wordToKey('cadb');
+ * @example sortWordByLetters('cadb');
  *          returns 'abcd'
  */
-const wordToKey = (word) => word.split('').sort().join('');
+const sortWordByLetters = (word) => word.split('').sort().join('');
 
 /**
- * Finding anagrams in array of strings  
+ * Finding anagrams in array of strings
  * @param {Array} words - array of words(string) where we are searching anagrams
  * @returns {Array} sorted array of arrays with anagrams
  * @example anagram(['cadb', 'bdac', 'edf', 'abc', 'def']);
@@ -38,15 +34,17 @@ const anagram = (words) => {
         throw "The input is incorrect!";
     }
     words.sort();
-    
-    let map = new Map();
-    
+
+    const mapOfAnagrams = new Map();
+
     words.forEach((word) => {
-        if (!map.has(wordToKey(word))) {
-            map.set(wordToKey(word), new Array())
+        const wordSortedByLetters = sortWordByLetters(word);
+
+        if (!mapOfAnagrams.has(wordSortedByLetters)) {
+            mapOfAnagrams.set(wordSortedByLetters, new Array());
         }
-        map.get(wordToKey(word)).push(word);
+        mapOfAnagrams.get(wordSortedByLetters).push(word);
     })
-    
-    return Array.from(map.values()).filter((elem) => elem.length > 1);
+
+    return Array.from(mapOfAnagrams.values()).filter((elem) => elem.length > 1);
 }
--- tests/anagram.js
+++ tests/anagram.js
@@ -1,106 +1,106 @@
 'use strict';
 
 QUnit.module('Тестируем функцию anagram', function () {
-	QUnit.test('Функция работает правильно', function (assert) {
-		const input = [
-			'кот', 'пила', 'барокко',
-			'стоп', 'ток', 'кошка',
-			'липа', 'коробка', 'пост'
-		];
+    QUnit.test('Функция работает правильно', function (assert) {
+        const input = [
+            'кот', 'пила', 'барокко',
+            'стоп', 'ток', 'кошка',
+            'липа', 'коробка', 'пост'
+        ];
 
-		const output = [
-			[ 'барокко', 'коробка' ],
-			[ 'кот', 'ток' ],
-			[ 'липа', 'пила' ],
-			[ 'пост', 'стоп' ]
-		];
+        const output = [
+            ['барокко', 'коробка'],
+            ['кот', 'ток'],
+            ['липа', 'пила'],
+            ['пост', 'стоп']
+        ];
 
-		assert.deepEqual(anagram(input), output);
-	});
+        assert.deepEqual(anagram(input), output);
+    });
 
-	QUnit.test('Доп тест на рускоязычные слова', function (assert) {
-		const input = [
-			'дорога', 'баян', 'кабан',
-			'банка', 'города', 'нитка',
-			'ткани', 'коробка', 'баня'
-		];
+    QUnit.test('Доп тест на рускоязычные слова', function (assert) {
+        const input = [
+            'дорога', 'баян', 'кабан',
+            'банка', 'города', 'нитка',
+            'ткани', 'коробка', 'баня'
+        ];
 
-		const output = [
-			[ 'банка', 'кабан' ],
-			[ 'баня', 'баян' ],
-			[ 'города', 'дорога' ],
-			[ 'нитка', 'ткани' ]
-		];
+        const output = [
+            ['банка', 'кабан'],
+            ['баня', 'баян'],
+            ['города', 'дорога'],
+            ['нитка', 'ткани']
+        ];
 
-		assert.deepEqual(anagram(input), output);
-	});
-	
-	QUnit.test('Тест на тройные анаграммы', function (assert) {
-		const input = [
-			'парадокс', 'марочник', 'романчик',
-			'распадок', 'просадка', 'курсант',
-			'струнка', 'рамочник', 'баня'
-		];
+        assert.deepEqual(anagram(input), output);
+    });
 
-		const output = [
-			[ 'курсант', 'струнка' ],
-			[ 'марочник', 'рамочник', 'романчик' ],
-			[ 'парадокс', 'просадка', 'распадок' ]
-		];
+    QUnit.test('Тест на тройные анаграммы', function (assert) {
+        const input = [
+            'парадокс', 'марочник', 'романчик',
+            'распадок', 'просадка', 'курсант',
+            'струнка', 'рамочник', 'баня'
+        ];
 
-		assert.deepEqual(anagram(input), output);
-	});
+        const output = [
+            ['курсант', 'струнка'],
+            ['марочник', 'рамочник', 'романчик'],
+            ['парадокс', 'просадка', 'распадок']
+        ];
 
-	QUnit.test('Тест на неверные входные данные', function (assert) {
-		const input = 770;
-		const output = "The input is incorrect!"
+        assert.deepEqual(anagram(input), output);
+    });
 
-		try {
-			anagram(input)
-		} catch (error) {
-			assert.deepEqual(error, output);
-		}
-	});
+    QUnit.test('Тест на неверные входные данные', function (assert) {
+        const input = 770;
+        const output = "The input is incorrect!";
 
-	QUnit.test('Функция работает правильно с англ словами', function (assert) {
-		const input = [
-			'listen', 'restful', 'santa', 'vile',
-			'fluster', 'silent', 'evil',  'satan'
-		];
+        try {
+            anagram(input);
+        } catch (error) {
+            assert.deepEqual(error, output);
+        }
+    });
 
-		const output = [
-			[ 'evil', 'vile' ],
-			['fluster', 'restful'],
-			['listen', 'silent'],
-			[ 'santa', 'satan' ]
-		];
+    QUnit.test('Функция работает правильно с англ словами', function (assert) {
+        const input = [
+            'listen', 'restful', 'santa', 'vile',
+            'fluster', 'silent', 'evil', 'satan'
+        ];
 
-		assert.deepEqual(anagram(input), output);
-	});
+        const output = [
+            ['evil', 'vile'],
+            ['fluster', 'restful'],
+            ['listen', 'silent'],
+            ['santa', 'satan']
+        ];
 
-	QUnit.test('Функция работает правильно со словосочетаниями и предложениями', function (assert) {
-		const input = [
-			'eleven plus two.', 'elegant man', 'over fifty',
-			'a gentleman', 'twelve plus one.', 'bad credit', 
-			'forty five', 'debit card', 'school master', 'the classroom'
-		];
+        assert.deepEqual(anagram(input), output);
+    });
 
-		const output = [
-			[ 'a gentleman', 'elegant man' ],
-			[ 'bad credit', 'debit card' ],
-			[ 'eleven plus two.', 'twelve plus one.' ],
-			[ 'forty five', 'over fifty' ],
-			[ 'school master', 'the classroom' ]
-		];
+    QUnit.test('Функция работает правильно со словосочетаниями и предложениями', function (assert) {
+        const input = [
+            'eleven plus two.', 'elegant man', 'over fifty',
+            'a gentleman', 'twelve plus one.', 'bad credit',
+            'forty five', 'debit card', 'school master', 'the classroom'
+        ];
 
-		assert.deepEqual(anagram(input), output);
-	});
+        const output = [
+            ['a gentleman', 'elegant man'],
+            ['bad credit', 'debit card'],
+            ['eleven plus two.', 'twelve plus one.'],
+            ['forty five', 'over fifty'],
+            ['school master', 'the classroom']
+        ];
 
-	QUnit.test('Функция работает правильно если нет анаграмм', function (assert) {
-		const input = ['eleven plus two.', 'elegant man', 'over fifty'];
+        assert.deepEqual(anagram(input), output);
+    });
 
-		const output = [];
+    QUnit.test('Функция работает правильно если нет анаграмм', function (assert) {
+        const input = ['eleven plus two.', 'elegant man', 'over fifty'];
 
-		assert.deepEqual(anagram(input), output);
-	});
+        const output = [];
+
+        assert.deepEqual(anagram(input), output);
+    });
 });
