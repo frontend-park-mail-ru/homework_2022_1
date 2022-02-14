18d6a2ce8a9e32893e53d52762664b033dd54fa5
rinatkh <khairullinrinat.99@gmail.com>
2022-02-14 23:07:08 +0300

    first

--- source/minmax.js
+++ source/minmax.js
@@ -1,11 +1,17 @@
 'use strict';
 
+/**
+ * find min and max value in string
+ * @param {string} strNumbers
+ * @return {[minValue, maxValue]}
+ */
+
 const minmax = (strNumbers) => {
-    strNumbers = strNumbers.replace(/[а-я\,\+]/gi, '')
+    strNumbers = strNumbers.replace(/[а-я\,\+]/gi, '');
     if (strNumbers.replace(/\s/g, '').length == 0)
-        return [undefined, undefined]
-    let numbers = strNumbers.split(' ')
-    let min = Math.min(...numbers)
-    let max = Math.max(...numbers)
-    return [min, max]
+        return [undefined, undefined];
+    let numbers = strNumbers.split(' ');
+    let min = Math.min(...numbers);
+    let max = Math.max(...numbers);
+    return [min, max];
 }
