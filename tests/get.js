e8b8f0130602bc91cf1fe8407090e479d6db2efc
papazloynt <cool.chastikov53@gmail.com>
2022-02-14 12:32:53 +0300

    change diff

--- tests/get.js
+++ tests/get.js
@@ -1,15 +1,15 @@
 'use strict';
 
 QUnit.module('Тестируем функцию get', function () {
-    QUnit.test('get работает с объектом на разных уровнях', function (assert) {
-        let obj = {
-            name: 'Vasa',
-            age: 47,
-            mother: {
-                name: 'Luda ot verbluda',
-                age: 105,
-            },
-        }
+    QUnit.test('get работает правильно c объектами с существующими свойствами', function (assert) {
+        const obj = {
+            foo: 'bar',
+            deep: {
+                hested: {
+                    field: 'baz'
+                }
+            }
+        };
          assert.strictEqual(get(obj, 'age'), 47, 'First level!');
          assert.strictEqual(get(obj, 'mother.name'), 'Luda ot verbluda', 'Second level!');
          assert.strictEqual(get(obj, 'name.length'), obj.name.length, 'Write len(name)');
