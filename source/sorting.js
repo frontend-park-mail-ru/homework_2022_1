376f7feb51b086f7ec6bb59c92a8c56f35401eb8
Мужецкий Антон <tonich2001@gmail.com>
2022-02-14 17:56:06 +0300

    fix: tutor review

--- source/sorting.js
+++ source/sorting.js
@@ -9,7 +9,7 @@
  * @param {Array} arr - исходный массив, который сортируется
  * @param {Array} keys - массив имен свойств, по которым проводится сортировка
  *
- * @returns {Array} arr - возвращает отсортированный массив
+ * @returns {Array} tmp - возвращает отсортированный массив
  */
 
 const sorting = (arr, keys) => {
@@ -21,20 +21,21 @@ const sorting = (arr, keys) => {
         return arr;
     }
 
-    keys.reverse().forEach(it => {
-        arr.sort((last, next) => { // sort stability is observed
+    const tmp = Object.assign([], [...arr]);
+
+    keys.reverse().forEach((it) => {
+        tmp.sort((last, next) => { // sort stability is observed
             if (typeof last[it] == 'number' || typeof next[it] == 'number') {
                 if (last[it] > next[it]) return 1;
                 if (last[it] < next[it]) return -1;
                 return 0;
             } else {
-                let collator = new Intl.Collator(undefined, {
+                return (new Intl.Collator(undefined, {
                     caseFirst: 'upper' // uppercase first
-                });
-                return collator.compare(last[it], next[it]);
+                })).compare(last[it], next[it]);
             }
         })
     });
 
-    return arr;
+    return tmp;
 }
