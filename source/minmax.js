ad6e6b05561e58439a29be8acc30bd1c28cd7d33
rinatkh <khairullinrinat.99@gmail.com>
2022-02-17 00:50:14 +0300

    fix #2

--- source/minmax.js
+++ source/minmax.js
@@ -2,16 +2,17 @@
 
 /**
  * find min and max value in string
- * @param {string} strNumbers
- * @returns {[minValue, maxValue]}
+ * @param {string} strNumbers - The string with numbers
+ * @returns {[minValue, maxValue]} - The max and the min of input string
  */
 
 const minmax = (strNumbers) => {
     strNumbers = strNumbers.replace(/[а-я\,\+]/gi, '');
-    if (strNumbers.replace(/\s/g, '').length == 0)
+    if (strNumbers.replace(/\s/g, '').length === 0) {
         return [undefined, undefined];
-    let numbers = strNumbers.split(' ');
-    let min = Math.min(...numbers);
-    let max = Math.max(...numbers);
+    }
+    const numbers = strNumbers.split(' ');
+    const min = Math.min(...numbers);
+    const max = Math.max(...numbers);
     return [min, max];
 }
