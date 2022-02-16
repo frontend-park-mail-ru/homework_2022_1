535e32369a66bc2d5bbba82f95b5e255a3840522
Nikita Schelkanov <nik.schelkanov@gmail.com>
2022-02-16 16:36:10 +0300

    change c-style to reduce

--- source/format.js
+++ source/format.js
@@ -9,21 +9,24 @@
 const format = (numbers, columnNumbers) => {
     let highestMultiIndent = [];
     let output = '';
-
+    
     for (let j = 0; j < columnNumbers; j++) {
         let highestIndent = 0;
-        for (let i = (0 + j); i < numbers.length; i += columnNumbers) {
-            highestIndent = (numbers[i].toString().length > highestIndent)? numbers[i].toString().length : highestIndent; 
-        }
+        numbers.reduce((previosValue, currentValue, currenIndex, array) => {
+            if (currenIndex === j || (currenIndex % columnNumbers) === j) {
+                highestIndent = (currentValue.toString().length > highestIndent)? currentValue.toString().length : highestIndent; 
+            }
+        }, 0);
+
         highestMultiIndent[j] = highestIndent;
     }
 
     for (let i = 0; i < numbers.length; i += columnNumbers) {
         for (let j = 0; j < columnNumbers; j++) {
-            if (numbers[i + j] !== undefined) {
-                let borderCase = (j == 0)? 0 : 1; 
-                output += ' '.repeat(highestMultiIndent[j] + borderCase - numbers[i + j].toString().length) + numbers[i + j];
-            }
+                if (numbers[i + j] !== undefined) {
+                    let borderCase = (j == 0)? 0 : 1;
+                    output += ' '.repeat(highestMultiIndent[j] + borderCase - numbers[i + j].toString().length) + numbers[i + j];
+                }
         }
         if ((i + columnNumbers) < numbers.length) {
             output += '\n';
