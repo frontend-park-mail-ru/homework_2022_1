4e60d604a37bb1fd931e40d7376e9b506c070270
Your Name <you@example.com>
2022-02-17 19:55:38 +0300

    fix codestyle

--- source/rle.js
+++ source/rle.js
@@ -1,3 +1,4 @@
+
 'use strict';
 /**
  * The function implements rle encoding
@@ -5,23 +6,25 @@
  * @returns {string} encoded string. e.g: 'A3B3'
  */
 const rle = (strNormal) => {
-    if (typeof strNormal !== 'string')
+    if (typeof strNormal !== 'string') {
         return '-1';
-    if (!/^[a-zA-Z]+$/.test(strNormal))
+    }
+    if (!/^[a-zA-Z]+$/.test(strNormal)) {
         return '-1';
+    }
     const strSplit = strNormal.split('');
     let count = 1;
     let strRes = '';
-    strRes = strSplit.reduce((strRes, curCh, index) => {
+    return strRes = strSplit.reduce((strRes, curCh, index) => {
         if (strRes[strRes.length - 1] === curCh) {
             ++count;
-            if (index === strSplit.length - 1)
+            if (index === strSplit.length - 1) {
                 return `${strRes}${count}`;
+            }
             return strRes;
         }
         const countTemp = count;
         count = 1;
         return `${strRes}${countTemp !== 1 ? countTemp : ''}${curCh}`;
       });
-    return strRes;
 };
