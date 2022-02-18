9ee83226e8a2a42216e9aee5efd29824124b9bf5
Роман Медников <jellybe@yandex.ru>
2022-02-18 22:00:40 +0300

    Исправления оформления кода.

--- source/plainify.js
+++ source/plainify.js
@@ -6,15 +6,13 @@
  * @returns {object} Plain-like object
  */
 const plainify = (obj) => {
-    if (typeof obj !== 'object' || Array.isArray(obj) || !obj)
-    {
+    if (typeof obj !== 'object' || Array.isArray(obj) || !obj) {
         throw new TypeError('Unsupported argument');
     }
     return Object.entries(obj).reduce((acc, prop) => {
         var [propKey, propVal] = prop;
         let res = {};
-        if (propVal && typeof propVal === 'object' && !Array.isArray(propVal))
-        {
+        if (propVal && typeof propVal === 'object' && !Array.isArray(propVal)) {
             Object.entries(plainify(propVal)).forEach((propDeep) => {
                 res[propKey+'.'+propDeep[0]] = propDeep[1];
             });
