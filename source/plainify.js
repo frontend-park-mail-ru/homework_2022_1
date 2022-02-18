d4a60480c9ecfdd944d47b74a8e139d77cc03e59
Роман Медников <jellybe@yandex.ru>
2022-02-18 20:40:01 +0300

    Убран аргумент ks и двойные кавычки.

--- source/plainify.js
+++ source/plainify.js
@@ -1,19 +1,25 @@
-'use strict';
-
 /**
  * Returns plain-like object given an object with nested properties.
  * @param {object} obj - Object with nested properties
  * @returns {object} Plain-like object
  */
-const plainify = (obj, ks = []) => {
+const plainify = (obj) => {
     if (typeof obj !== 'object' || Array.isArray(obj) || !obj)
     {
-        throw new TypeError("Unsupported argument");
+        throw new TypeError('Unsupported argument');
     }
-    return Object.keys(obj).reduce((acc, k1) => {
-        return Object.assign(acc,
-            (obj[k1] && typeof obj[k1] === 'object' && !Array.isArray(obj[k1]))
-            ? plainify(obj[k1], ks.concat(k1))
-            : {[ks.concat(k1).join('.')]: obj[k1]}
-        )}, {})
+    return Object.entries(obj).reduce((acc, kv) => {
+        var [k1, o1] = kv;
+        let res = {};
+        if (o1 && typeof o1 === 'object' && !Array.isArray(o1))
+        {
+            const tmp = plainify(o1);
+            Object.entries(tmp).forEach((kv2) => {
+                res[k1+'.'+kv2[0]] = kv2[1];
+            });
+        }
+        else
+            res[k1] = o1;
+        return Object.assign(acc, res)
+    }, {})
 }
